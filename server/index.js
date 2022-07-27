3adededfe88fce89202b15a4a7183bc45f3e2052
Kamerron Pace <youngkamman@gmail.com>
2022-07-27 16:37:40 -0400

    css fixed

--- server/index.js
+++ server/index.js
@@ -4,18 +4,18 @@ const path = require('path')
 
 const app = express()
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../index.html'))
-})
+// app.get('/', function(req, res) {
+//     res.sendFile(path.join(__dirname, '../index.html'))
+// })
 
 // app.get('/css', function(req, res) {
 //     res.sendFile(path.join(__dirname, '../styles.css'))
 // })
 
- app.use('/js', express.static(path.join(__dirname, '../styles.css')))
+ app.use(express.static(path.join(__dirname, '/../public')))
 
  app.get('/js', (req, res) => {
-    res.sendFile(path.join(__dirname, './server/index.js'))
+    res.sendFile(path.join(__dirname, '../public/index.html'))
   })
 const port = process.env.PORT  || 4005
 
